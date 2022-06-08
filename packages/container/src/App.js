75f85f33a577bf6dae8fcd338f86f2a42e9f7a5b
barathrajd <barathdeva757@gmail.com>
2022-06-08 15:48:36 +0530

    microfrontend with react

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingAppComponent from "./components/MarketingApp";
+
+export default () => {
+  return (
+    <div>
+      <h1>Hello There</h1>
+      <hr />
+      <MarketingAppComponent />
+    </div>
+  );
+};
